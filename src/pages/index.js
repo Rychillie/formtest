6ace162b58d5ceb7dc50a77e986cfddda43576c2
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-05-20 00:23:08 -0300

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import React from "react"
 
 const Home = () => (
-    <form name="contact" method="POST" data-netlify="true" action="/" >
-        <p>
-            <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
+    <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <input type="text" name="name" placeholder="Name"/>
+        <input type="email" name="email" placeholder="Email"/>
+      </div>
+      <div >
+        <textarea name="message" placeholder="Mensagem"/>
+      </div>
+      <button type="submit">Enviar</button>
     </form>
 )
 
