3e53b1438e34a2577b9d9a15e2fc8f169c326860
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-05-19 23:53:12 -0300

    default

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import React from "react"
 
-export default () => <form name="contact" method="POST" data-netlify="true">
-<p>
-  <label>Your Name: <input type="text" name="name" /></label>
-</p>
-<p>
-  <label>Your Email: <input type="email" name="email" /></label>
-</p>
-<p>
-  <label>Message: <textarea name="message"></textarea></label>
-</p>
-<p>
-  <button type="submit" download="Ebook-Free.docx">Send</button>
-</p>
-</form>
+const Home = () => (
+    <form name="contact" method="POST" data-netlify="true">
+        <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
+)
+
+export default Home
