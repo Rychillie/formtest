ffee030ebe575666aab3ad850841b0f16fc2d353
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-05-19 23:42:23 -0300

    test oneclick

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default () => <form name="contact" method="POST" data-netlify="true" download="Ebook-Free.docx">
+export default () => <form name="contact" method="POST" data-netlify="true">
 <p>
   <label>Your Name: <input type="text" name="name" /></label>
 </p>
@@ -8,15 +8,9 @@ export default () => <form name="contact" method="POST" data-netlify="true" down
   <label>Your Email: <input type="email" name="email" /></label>
 </p>
 <p>
-  <label>Your Role: <select name="role[]" multiple>
-    <option value="leader">Leader</option>
-    <option value="follower">Follower</option>
-  </select></label>
-</p>
-<p>
   <label>Message: <textarea name="message"></textarea></label>
 </p>
 <p>
-  <button type="submit">Send</button>
+  <button type="submit" onclick="window.open('Ebook-Free.docx')">Send</button>
 </p>
 </form>
